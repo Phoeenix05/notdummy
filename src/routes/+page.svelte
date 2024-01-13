d9b014fda93c6932335da1115a4543245430e9f4
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2024-01-13 17:38:13 +0200

    feat: ability to add fields
    though no export functionality yet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { Output } from 'valibot'
+	import { object, parse, picklist, string } from 'valibot'
+
+	const fieldTypes = ['text', 'uuid', 'name', 'id']
+
+	const FieldSchema = object({
+		name: string(),
+		type: picklist(fieldTypes)
+	})
+	type FieldSchemaT = Output<typeof FieldSchema>
+
+	let fields: { id: number; data: FieldSchemaT }[] = []
+	$: console.log(fields)
+
+	let fieldName = ''
+	let fieldType = fieldTypes[0]
+
+	const addField = () => {
+		if (fieldName == '') {
+			return
+		}
+
+		const data: FieldSchemaT = {
+			name: fieldName,
+			type: fieldType
+		}
+
+		fields = [...fields, { id: fields.length, data: parse(FieldSchema, data) }]
+	}
+</script>
+
+<div>
+	<select bind:value={fieldType} name="type" id="ftype">
+		{#each fieldTypes as value}
+			<option {value}>{value}</option>
+		{/each}
+	</select>
+
+	<input bind:value={fieldName} type="text" />
+</div>
+
+<button
+	on:click={() => {
+		addField()
+	}}>add</button
+>
+
+{#each fields as field}
+	<div id={field.id.toString()}>{field.data.name}, {field.data.type}</div>
+{/each}
